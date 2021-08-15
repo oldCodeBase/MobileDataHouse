a2b44c5caf738f07966c6fee5ecb97ff0b1bd169
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-15 20:33:04 +0300

    Initial Commit

---
+++ MobileDataHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileDataHouse
+//
+//  Created by Ibragim Akaev on 15/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
