69d50a21cafa32317a506e623fd9d5e520a44eb7
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-17 13:15:20 +0300

    Create model for app

---
+++ MobileDataHouse/Models/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  MobileDataHouse
+//
+//  Created by Ibragim Akaev on 15/08/2021.
+//
+
+import Foundation
+
+struct Contact {
+    let fullName: String
+    let number: String
+}
