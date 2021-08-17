4e23a2cff685caad498e8b72bdb902c93fe66be5
Ibragim Akaev <akaev_ibragim@mail.ru>
2021-08-17 13:13:44 +0300

    Add useful extensions for UITableView

---
+++ MobileDataHouse/Extensions/UITableView + Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UITableView + Extensions.swift
+//  MobileDataHouse
+//
+//  Created by Ibragim Akaev on 15/08/2021.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func register<T: UITableViewCell>(_: T.Type) {
+        register(T.self, forCellReuseIdentifier: String(describing: T.self))
+    }
+    
+    func dequeue<T: UITableViewCell>(_: T.Type, indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as! T
+    }
+}
